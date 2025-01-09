a5fd5970dbdd038c7ad7977d888a38a831f08eba
MasonWen028 <funrie028@gmail.com>
2025-01-09 17:24:27 +1030

    feat(homepageservice): implement homepage service

---
+++ Auraloop/NeteaseCloudMusicSDK/Services/HomepageService.cs
@@ -0,0 +1,49 @@
+﻿using NeteaseCloudMusicApi_SDK.Interfaces;
+using NeteaseCloudMusicSDK.ApiClient;
+using NeteaseCloudMusicSDK.ApiClient.Models;
+using NeteaseCloudMusicSDK.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeteaseCloudMusicSDK.Services
+{
+    public class HomepageService : IHomepageService
+    {
+        private readonly NetEaseApiClient _client;
+
+        public HomepageService(NetEaseApiClient client)
+        {
+            _client = client;            
+        }
+
+        /// <inberitdoc/>
+        public async Task<ApiResponse> GetBlockPage(HomepageBlockPageRequestModel requestModel)
+        {
+            try
+            {
+                var options = new RequestOptions($"/api/homepage/block/page", requestModel, "weapi");
+                return await _client.HandleRequestAsync(options);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to get blocked pages.", ex);
+            }
+        }
+
+        /// <inberitdoc/>
+        public async Task<ApiResponse> GetDragonBall()
+        {
+            try
+            {
+                var options = new RequestOptions($"/api/homepage/dragon/ball/static");
+                return await _client.HandleRequestAsync(options);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to get blocked pages.", ex);
+            }
+        }
+    }
+}
