2ca0162fefcc965d61392d9be35dc881ad64d606
MasonWen028 <funrie028@gmail.com>
2025-01-09 21:37:03 +1030

    feat(music-service): implement musicservice

---
+++ Auraloop/NeteaseCloudMusicSDK/Services/MusicService.cs
@@ -0,0 +1,40 @@
+﻿using NeteaseCloudMusicApi_SDK.Interfaces;
+using NeteaseCloudMusicSDK.ApiClient;
+using NeteaseCloudMusicSDK.ApiClient.Models;
+using NeteaseCloudMusicSDK.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeteaseCloudMusicSDK.Services
+{
+    public class MusicService : IMusicService
+    {
+        private readonly NetEaseApiClient _client;
+
+        public MusicService(NetEaseApiClient client)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// Fetch the process of song listened last time
+        /// </summary>
+        /// <param name="SongId"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<ApiResponse> StartMusicListening(long  songId)
+        {
+            try
+            {
+                var options = new RequestOptions($"/api/content/activity/music/first/listen/info", new { songId = songId });
+                return await _client.HandleRequestAsync(options);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to get process of song with ID: {songId}.", ex);
+            }
+        }
+    }
+}
