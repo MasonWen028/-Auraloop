8c18beba9c2cbc39fb8791afbc0b505597a072d1
MasonWen028 <funrie028@gmail.com>
2025-01-09 17:09:14 +1030

    feat(history service): implement history service

---
+++ Auraloop/NeteaseCloudMusicSDK/Services/HistoryService.cs
@@ -0,0 +1,49 @@
+﻿using NeteaseCloudMusicApi_SDK.Interfaces;
+using NeteaseCloudMusicSDK.ApiClient;
+using NeteaseCloudMusicSDK.ApiClient.Models;
+using NeteaseCloudMusicSDK.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeteaseCloudMusicSDK.Services
+{
+    public class HistoryService: IHistoryService
+    {
+        private readonly NetEaseApiClient _client;
+
+        public HistoryService(NetEaseApiClient client)
+        {
+            _client = client;
+        }
+
+        /// <inheritdoc/>
+        public async Task<ApiResponse> GetRecommendedSongs()
+        {
+            try
+            {
+                var options = new RequestOptions($"/api/discovery/recommend/songs/history/recent", null, "weapi");
+                return await _client.HandleRequestAsync(options);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to get album subscription list.", ex);
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task<ApiResponse> GetRecommendedSongsDetail(HistoryRecommendSongsDetailRequestModel requestModel)
+        {
+            try
+            {
+                var options = new RequestOptions($"/api/discovery/recommend/songs/history/detail", requestModel, "weapi");
+                return await _client.HandleRequestAsync(options);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to get album subscription list.", ex);
+            }
+        }
+    }
+}
