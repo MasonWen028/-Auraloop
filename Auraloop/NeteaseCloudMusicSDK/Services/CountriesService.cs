a90479f7d90a6962729329199c3a9ceae154e424
MasonWen028 <funrie028@gmail.com>
2025-01-09 15:52:12 +1030

    feat(countriesservice): implement countriesService for fetch countries and regions code.

---
+++ Auraloop/NeteaseCloudMusicSDK/Services/CountriesService.cs
@@ -0,0 +1,35 @@
+﻿using NeteaseCloudMusicApi_SDK.Interfaces;
+using NeteaseCloudMusicSDK.ApiClient;
+using NeteaseCloudMusicSDK.ApiClient.Models;
+using NeteaseCloudMusicSDK.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeteaseCloudMusicSDK.Services
+{
+    public class CountriesService: ICountriesService
+    {
+        private readonly NetEaseApiClient _client;
+
+        public CountriesService(NetEaseApiClient client)
+        {
+            _client = client;
+        }
+
+        /// <inheritdoc/>
+        public async Task<ApiResponse> CountriesCodeList()
+        {
+            try
+            {
+                var options = new RequestOptions($"/api/lbs/countries/v1");
+                return await _client.HandleRequestAsync(options);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to get countries and regions code.", ex);
+            }
+        }
+    }
+}
