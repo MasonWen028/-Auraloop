b7f60b9fcf4e7c767b2338e8fb7ac110d393e3ea
MasonWen028 <funrie028@gmail.com>
2025-01-12 00:30:31 +1030

    feat(pl-service): implement plservice

---
+++ Auraloop/NeteaseCloudMusicSDK/Services/PlService.cs
@@ -0,0 +1,39 @@
+﻿using NeteaseCloudMusicApi_SDK.Interfaces;
+using NeteaseCloudMusicSDK.ApiClient;
+using NeteaseCloudMusicSDK.ApiClient.Models;
+using NeteaseCloudMusicSDK.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeteaseCloudMusicSDK.Services
+{
+    public class PlService : IPlService
+    {
+        private readonly NetEaseApiClient _client;
+
+        public PlService(NetEaseApiClient client)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// Fetch private message and notices
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<ApiResponse> Count()
+        {
+            try
+            {
+                var options = new RequestOptions($"/api/pl/count", null, "weapi");
+                return await _client.HandleRequestAsync(options);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to fetch private message and notices", ex);
+            }
+        }
+    }
+}
