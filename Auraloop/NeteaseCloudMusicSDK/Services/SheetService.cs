ff46410794742a98391755c702713eb34ba63c4d
MasonWen028 <funrie028@gmail.com>
2025-01-12 14:23:13 +1030

    feat(sheet-service): implement sheetservice

---
+++ Auraloop/NeteaseCloudMusicSDK/Services/SheetService.cs
@@ -0,0 +1,51 @@
+﻿using NeteaseCloudMusicApi_SDK.Interfaces;
+using NeteaseCloudMusicSDK.ApiClient;
+using NeteaseCloudMusicSDK.ApiClient.Models;
+using NeteaseCloudMusicSDK.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeteaseCloudMusicSDK.Services
+{
+    public class SheetService : ISheetService
+    {
+        private readonly NetEaseApiClient _client;
+
+        public SheetService(NetEaseApiClient client)
+        {
+            _client = client;
+        }
+
+        /// <inheritdoc/>
+        public async Task<ApiResponse> List(SheetListRequestModel requestModel)
+        {
+            try
+            {
+                var option = new RequestOptions("/api/music/sheet/list/v1", requestModel);
+                return await _client.HandleRequestAsync(option);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception($"Failed to fetch sheet list with ID: {requestModel.Id}", ex);
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task<ApiResponse> Preview(SheetPreviewRequestModel requestModel)
+        {
+            try
+            {
+                var option = new RequestOptions("/api/music/sheet/preview/info", requestModel);
+                return await _client.HandleRequestAsync(option);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception($"Failed to preview sheet with ID: {requestModel.Id}", ex);
+            }
+        }
+    }
+}
