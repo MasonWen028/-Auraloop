7ebbd8fa347d4b9aae6636d279a9ac2320862caf
MasonWen028 <funrie028@gmail.com>
2025-01-12 22:46:05 +1030

    feat(threshold-service): implement theshsholdservice

---
+++ Auraloop/NeteaseCloudMusicSDK/Services/ThresholdService.cs
@@ -0,0 +1,40 @@
+﻿using NeteaseCloudMusicApi_SDK.Interfaces;
+using NeteaseCloudMusicSDK.ApiClient;
+using NeteaseCloudMusicSDK.ApiClient.Models;
+using NeteaseCloudMusicSDK.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeteaseCloudMusicSDK.Services
+{
+    public class ThresholdService : IThresholdService
+    {
+        private readonly NetEaseApiClient _client;
+
+        public ThresholdService(NetEaseApiClient client)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// Fetch threshold details
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<ApiResponse> ThresholdDetailGet()
+        {
+            try
+            {
+                var option = new RequestOptions("/api/influencer/web/apply/threshold/detail/get");
+
+                return await _client.HandleRequestAsync(option);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to fetch threshold details", ex);
+            }
+        }
+    }
+}
