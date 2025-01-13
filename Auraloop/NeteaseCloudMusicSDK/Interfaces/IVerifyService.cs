5bfb2cc0d0a78f1e0b503b3846076edcd492acc1
MasonWen028 <funrie028@gmail.com>
2025-01-13 11:58:54 +1030

    feat(verify-service): implement verifyservice

--- Auraloop/NeteaseCloudMusicSDK/Interfaces/IVerifyService.cs
+++ Auraloop/NeteaseCloudMusicSDK/Interfaces/IVerifyService.cs
@@ -9,17 +9,11 @@ namespace NeteaseCloudMusicApi_SDK.Interfaces
     public interface IVerifyService
     {
         /// <summary>
-        /// Generates a QR code for user verification.
+        /// Check the status of qr code key
         /// </summary>
-        /// <param name="requestModel">The model containing parameters required to generate the QR code.</param>
-        /// <returns>An <see cref="ApiResponse"/> containing the QR code, its URL, and a base64-encoded image.</returns>
-        Task<ApiResponse> GetQrCode(VerifyGetQrRequestModel requestModel);
-
-        /// <summary>
-        /// Checks the status of a previously generated QR code.
-        /// </summary>
-        /// <param name="qrCode">The QR code to check the status for.</param>
-        /// <returns>An <see cref="ApiResponse"/> indicating the status of the QR code.</returns>
+        /// <param name="qrCode"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         Task<ApiResponse> CheckQrCodeStatus(string qrCode);
     }
 }
---
+++ Auraloop/NeteaseCloudMusicSDK/Services/VerifyService.cs
@@ -0,0 +1,40 @@
+﻿using NeteaseCloudMusicApi_SDK.Interfaces;
+using NeteaseCloudMusicSDK.ApiClient;
+using NeteaseCloudMusicSDK.ApiClient.Models;
+using NeteaseCloudMusicSDK.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeteaseCloudMusicSDK.Services
+{
+    public class VerifyService : IVerifyService
+    {
+        private readonly NetEaseApiClient _client;
+
+        public VerifyService(NetEaseApiClient client)
+        {  
+            _client = client; 
+        }
+
+        /// <summary>
+        /// Check the status of qr code key
+        /// </summary>
+        /// <param name="qrCode"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<ApiResponse> CheckQrCodeStatus(string qrCode)
+        {
+            try
+            {
+                var option = new RequestOptions("/api/frontrisk/verify/qrcodestatus", new { qrCode }, "weapi");
+                return await _client.HandleRequestAsync(option);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to verify the qr code: {qrCode}", ex);
+            }
+        }
+    }
+}
